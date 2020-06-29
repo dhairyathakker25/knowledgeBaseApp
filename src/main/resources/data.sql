797695173d17c68eff162edc196961773ceb1925
dhairyathakker25 <dhairyathakker25@gmail.com>
2020-06-29 21:29:49 +0100

    added h2 db properties and sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into topic_table values(1, 'spring boot description', 'Spring boot');
+insert into topic_table values(2, 'spring jap description', 'Spring jpa');
+insert into topic_table values(3, 'spring hibernate description', 'Spring hibernate');
